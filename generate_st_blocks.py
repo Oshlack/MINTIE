fcc5ed2b46c51352a446ab45e4b09593abc83eaa
Marek Cmero <marek.cmero@bioinf1.meerkat.mcri.edu.au>
2018-04-19 17:04:24 +1000

    Insert novel sequences into supertranscript reference

--- generate_st_blocks.py
+++ generate_st_blocks.py
@@ -5,28 +5,16 @@
 import argparse
 import pandas as pd
 import numpy as np
-import pysam
-import os
-import re
 import gffutils
-from Bio import SeqIO
 from intervaltree import Interval, IntervalTree
 
-import ipdb
-
 parser = argparse.ArgumentParser()
-#parser.add_argument(dest='samfile',
-#                    help='''SAM or BAM format file containing contig alignments''')
-#parser.add_argument(dest='novel_contigs_file',
-#                    help='''Annotated novel contigs file''')
 parser.add_argument(dest='gff_file',
                     help='''GTF reference annotation''')
 parser.add_argument(dest='out_bed',
                     help='''Output bed file of exonic coordinates.''')
 
 args        = parser.parse_args()
-#samfile     = args.samfile
-#nc_file     = args.novel_contigs_file
 gff_file    = args.gff_file
 out_bed     = args.out_bed
 
@@ -52,28 +40,10 @@ for chrom in exons:
         exon_df.extend(intervals)
 
 exon_df = pd.DataFrame(exon_df).drop_duplicates()
-exon_df = exon_df.groupby([0,1,2,3,5,6])[4].apply(lambda x: ','.join(x)).to_frame().reset_index()
+exon_df = exon_df.groupby([0,1,2,3,5,6])[4].apply(lambda x: '|'.join(x)).to_frame().reset_index()
 exon_df.columns = ['gene', 'chrom', 'start', 'end', 'score', 'strand', 'exons']
 
 exon_df['name'] = exon_df.gene + ' ' + exon_df.exons
 exon_df = exon_df[['chrom', 'start', 'end', 'name', 'score', 'strand']]
 exon_df.start = exon_df.start - 1 #bed coordinate offset (0-based)
 exon_df.to_csv(out_bed, sep='\t', index=False, header=False)
-
-#novel_contigs = pd.read_csv(nc_file, sep='\t')
-#novel_contigs = novel_contigs[novel_contigs.variant != 'soft-clip']
-
-#bamf = pysam.AlignmentFile(samfile)
-#reads = pysam.IndexedReads(bamf)
-#reads.build()
-
-#gene_out = pd.DataFrame()
-#genes = np.unique(novel_contigs.gene.values)
-#for gene in genes:
-#    nc = novel_contigs[novel_contigs.gene == gene]
-#    if any(nc.contig_varsize.values > 0):
-#        ipdb.set_trace()
-#
-#    else:
-#        gene_out = gene_out.append(exon_df[exon_df.gene == gene])
-#    #contig_mapping = reads.find(contig_row['contig'])
--- make_supertranscript_ref.py
+++ make_supertranscript_ref.py
@@ -5,45 +5,46 @@
 import argparse
 import pandas as pd
 import numpy as np
-import pysam
-import os
-import re
 import gffutils
 from Bio import SeqIO
-from intervaltree import Interval, IntervalTree
 
 import ipdb
 
 parser = argparse.ArgumentParser()
-parser.add_argument(dest='samfile',
-                    help='''SAM or BAM format file containing contig alignments''')
+parser.add_argument(dest='contigs_fasta',
+                    help='''Fasta file containing contig sequences''')
 parser.add_argument(dest='novel_contigs_file',
                     help='''Annotated novel contigs file''')
-parser.add_argument(dest='blocks',
+parser.add_argument(dest='block_bed',
                     help='''Sequence blocks used to construct transcript sequences.''')
-parser.add_argument(dest='fasta',
+parser.add_argument(dest='block_fasta',
                     help='''Fasta file containing sequences (contructed from the blocks bed file)''')
+parser.add_argument(dest='output_fasta',
+                    help='''Output supertranscript file''')
 
 args        = parser.parse_args()
-samfile     = args.samfile
+con_fasta   = args.contigs_fasta
 nc_file     = args.novel_contigs_file
-blocks      = args.blocks
-fasta_file  = args.fasta
+blocks      = args.block_bed
+block_fasta = args.block_fasta
+st_file     = args.output_fasta
 
 novel_contigs = pd.read_csv(nc_file, sep='\t')
 #novel_contigs = novel_contigs[novel_contigs.variant != 'soft-clip']
 exon_df =  pd.read_csv(blocks, sep='\t', header = None, names = ['chrom', 'start', 'end', 'name', 'value', 'strand'])
 exon_df[['gene', 'blocks']] = exon_df['name'].str.split(' ', expand=True)
 
-seqs = {}
-handle = open(fasta_file, 'r')
+block_seqs = {}
+handle = open(block_fasta, 'r')
 for record in SeqIO.parse(handle, 'fasta'):
-    seqs[record.id] = str(record.seq)
+    block_seqs[record.id] = str(record.seq)
 handle.close()
 
-bamf = pysam.AlignmentFile(samfile)
-reads = pysam.IndexedReads(bamf)
-reads.build()
+contig_seqs = {}
+handle = open(con_fasta, 'r')
+for record in SeqIO.parse(handle, 'fasta'):
+    contig_seqs[record.id] = record.seq
+handle.close()
 
 gene_out = pd.DataFrame()
 genes = np.unique([gn for gene in novel_contigs.gene.values for gn in gene.split('|')])
@@ -57,11 +58,50 @@ for gene in genes:
     contigs_with_novel_bits = nc[nc.contig_varsize.values > 0]
     if any(nc.contig_varsize.values > 0):
         for idx, contig_row in contigs_with_novel_bits.iterrows():
-            contig_mapping = reads.find(contig_row['contig'])
-            seqs = [read.query_sequence for read in contig_mapping]
-            #ipdb.set_trace()
+            seq = contig_seqs[contig_row['contig']]
+            csize = contig_row.contig_varsize
+            start = (len(seq) - contig_row.contig_pos2) if antisense else contig_row.contig_pos1
+            end = start + csize
 
+            gpos1, gpos2 = contig_row.genome_pos1, contig_row.genome_pos2
+            blocks_affected = gene_out[np.logical_and(gene_out.start <= gpos1, gene_out.end >= gpos2)]
+
+            chrom = 'MT' if contig_row.chrom1 == 'chrM' else contig_row.chrom1.split('chr')[1]
+            novel_seq = seq[start:end]
+            novel_seq_info = (chrom, gpos1, gpos2, gene_out.strand.values[0])
+            block_seqs['%s:%d-%d(%s)' % novel_seq_info] = novel_seq
+
+            if len(blocks_affected) > 1:
+                # this shouldn't happen.....
+                ipdb.set_trace()
+            for idx, block in blocks_affected.iterrows():
+                seq = block_seqs['%s:%d-%d(%s)' % (chrom, block.start+1, block.end, block.strand)]
+                left_seq = seq[:block.end-gpos2] if antisense else seq[:gpos1-block.start]
+                right_seq = seq[block.end-gpos1:] if antisense else seq[gpos2-block.start:]
+                block_seqs['%s:%d-%d(%s)' % (chrom, block.start+1, gpos1, block.strand)] = left_seq
+                block_seqs['%s:%d-%d(%s)' % (chrom, gpos2+1, block.end, block.strand)] = right_seq
+
+                left_block = pd.DataFrame([[block.chrom, block.start, gpos1, block['name'], block.value, block.strand, block.gene, block['blocks']]], columns=gene_out.columns)
+                right_block = pd.DataFrame([[block.chrom, gpos2, block.end, block['name'], block.value, block.strand, block.gene, block['blocks']]], columns=gene_out.columns)
+                novel_block = pd.DataFrame([[block.chrom, gpos1-1, gpos2, '%s novel' % gene, block.value, block.strand, block.gene, 'novel']], columns=gene_out.columns)
+
+                gene_out = gene_out[gene_out.index!=block.name]
+                gene_out = gene_out.append([left_block, novel_block, right_block], ignore_index=True)
+
+    gene_out = gene_out.sort_values(by=['start', 'end'], ascending=False) if antisense else gene_out.sort_values(by=['start','end'])
+    segs, seqs, names = [], [], []
+    seg_start = 1
     for idx,x in gene_out.iterrows():
+        names.append(x['blocks'])
         chrom = 'MT' if x.chrom == 'chrM' else x.chrom.split('chr')[1]
-        seq = seqs['%s:%d-%d(%s)' % (chrom, x.start+1, x.end, x.strand)]
+        seq = str(block_seqs['%s:%d-%d(%s)' % (chrom, x.start+1, x.end, x.strand)])
+        seqs.append(seq)
 
+        seg_end = seg_start + len(seq)
+        segs.append('%d-%d' % (seg_start, seg_end))
+        seg_start = seg_end+1
+
+    header = '>%s segs:%s names:%s\n' % (gene, ','.join(segs), ','.join(names))
+    sequence = ''.join(seqs) + '\n'
+    with open(st_file, 'a') as st_fasta:
+        st_fasta.writelines([header, sequence])
