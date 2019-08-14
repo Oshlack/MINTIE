41a291fc1f6539e05a553ee13b571b97789b73eb
Marek Cmero <cmerom@gmail.com>
2019-08-14 03:38:13 +0000

    Added test data and run script

---
+++ test/run_test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# modify reference to control fasta
+grep -v allvars-control references.groovy > backup_references.groovy
+echo "\"trans_fasta=$PWD/test/data/allvars-control.fasta\"" > tmp.txt
+cat backup_references.groovy tmp.txt > references.groovy ; rm tmp.txt
+
+cases=`ls test/data/cases/*gz`
+controls=`ls test/data/controls/*gz`
+tools/bin/bpipe @test/test_params.txt MINTIE.groovy $cases $controls
+
+# restore original references
+mv backup_references.groovy references.groovy
