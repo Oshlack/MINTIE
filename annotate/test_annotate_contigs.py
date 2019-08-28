66528e4bde20337750c4fcec7cd300d8f57e5924
Marek Cmero <cmerom@gmail.com>
2019-08-28 10:24:39 +1000

    Test get_juncs

--- annotate/test_annotate_contigs.py
+++ annotate/test_annotate_contigs.py
@@ -14,3 +14,11 @@ def test_get_next_letter(letter, next_letter):
 def test_get_next_id():
     assert ac.get_next_id('k49_123') == 'k49_123a'
     assert ac.get_next_id('k49_123') == 'k49_123b'
+
+def test_get_juncs():
+    tx_in = {'chrom': '1',
+             'exonStarts': '100,300,400',
+             'exonEnds': '200,350,600'}
+    result = [('1', '200', '300'),
+              ('1', '350', '400')]
+    assert ac.get_juncs(tx_in) == result
