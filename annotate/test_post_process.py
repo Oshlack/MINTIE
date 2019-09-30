b7508290912f6e7030daac78abe506f1d6ef77be
Marek Cmero <cmerom@gmail.com>
2019-09-30 10:07:44 +1000

    Added tests

--- annotate/test_post_process.py
+++ annotate/test_post_process.py
@@ -1,9 +1,20 @@
 import pytest
+import pandas as pd
 import post_process as pp
 
-@pytest.mark.parametrize('input,expected', [('A', ['A']),
+@pytest.mark.parametrize('gene,expected', [('A', ['A']),
                                             ('A|B', ['A', 'B']),
                                             ('A:B', ['A', 'B']),
                                             ('A:B|C', ['A', 'B', 'C'])])
-def test_get_all_genes(input, expected):
-    pp.get_all_genes(input) == expected
+def test_get_all_genes(gene, expected):
+    pp.get_all_genes(gene) == expected
+
+def test_filter_by_gene():
+    gene_filter = {0: ['B', 'C', 'E', 'X']}
+    gene_filter = pd.DataFrame.from_dict(gene_filter)
+
+    contigs = {'overlapping_genes': ['A', 'A:B', 'C|E', 'Y', 'X']}
+    contigs = pd.DataFrame.from_dict(contigs)
+
+    result = pp.filter_by_gene(contigs, gene_filter).overlapping_genes.values
+    assert list(result) == ['A:B', 'C|E', 'X']
