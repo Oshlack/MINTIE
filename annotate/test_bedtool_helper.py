f2a44166fe4dd56f9afb70cb5ab2e9fbf93858e2
Marek Cmero <cmerom@gmail.com>
2019-10-23 12:11:11 +1100

    Added tests to bedtool_helper
    - test get_block_seqs function

--- annotate/test_bedtool_helper.py
+++ annotate/test_bedtool_helper.py
@@ -1,19 +1,23 @@
 import pytest
 import bedtool_helper as bh
+import numpy as np
 import pandas as pd
+import tempfile
 from pybedtools import BedTool
 
+# test data
+gtf_data = {'chrom': ['1', '1', '1'],
+       'source': ['test', 'test', 'test'],
+       'feature': ['gene', 'exon', 'exon'],
+       'start': [100, 100, 200],
+       'end': [250, 150, 250],
+       'score': ['.', '.', '.'],
+       'strand': ['+', '+', '+'],
+       'frame': ['.', '.', '.'],
+       'attribute': ['', '', '']}
+
 def test_subset_featuretypes():
-    gtf = {'chrom': ['1', '1', '1'],
-           'source': ['test', 'test', 'test'],
-           'feature': ['gene', 'exon', 'exon'],
-           'start': [100, 100, 200],
-           'end': [250, 150, 250],
-           'score': ['.', '.', '.'],
-           'strand': ['+', '+', '+'],
-           'frame': ['.', '.', '.'],
-           'attribute': ['', '', '']}
-    gtf = pd.DataFrame.from_dict(gtf)
+    gtf = pd.DataFrame.from_dict(gtf_data)
     g = BedTool.from_dataframe(gtf)
     exons = BedTool(bh.subset_featuretypes(g, 'exon'))
     assert [e[2] for e in exons] == ['exon', 'exon']
@@ -26,3 +30,17 @@ def test_add_strand():
     b = BedTool.from_dataframe(bed).remove_invalid().sort().merge()
     bex = b.each(bh.add_strand, '+')
     assert [x.strand for x in bex] == ['+', '+']
+
+def test_get_block_seqs():
+    with tempfile.NamedTemporaryFile() as fa_tmp:
+        seq = np.random.choice(list('AGTC'), 400)
+        seq = ''.join(seq)
+        fa_tmp.write(bytes('>1\n', 'utf-8'))
+        fa_tmp.write(bytes(seq, 'utf-8'))
+        fa_tmp.flush()
+
+        gtf = pd.DataFrame.from_dict(gtf_data)
+        g = BedTool.from_dataframe(gtf)
+        g = g.sequence(fi=fa_tmp.name, s=True)
+        block_seqs = bh.get_block_seqs(g)
+        assert len(block_seqs) == 3
